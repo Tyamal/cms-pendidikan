af19c2703dffa53d6dd44a9161811fe98e4565d8
Mal <anityakumala@gmail.com>
2025-03-14 18:02:05 +0700

    Create Header.js

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>CMS Pendidikan</h1>
+            <nav>
+                <ul>
+                    <li><a href="/">Beranda</a></li>
+                    <li><a href="/login">Login</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
