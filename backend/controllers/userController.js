4fd6e1401c3e52e57dc095eaed5cbd78130fe47f
Mal <anityakumala@gmail.com>
2025-03-14 18:06:48 +0700

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,3 +6,10 @@ exports.createUser  = async (req, res) => {
     const user = new User({ username, password, role });
     await user.save();
     res.status(201).json(user);
+};
+
+// Fungsi untuk mendapatkan semua pengguna
+exports.getUsers = async (req, res) => {
+    const users = await User.find();
+    res.status(200).json(users);
+};
