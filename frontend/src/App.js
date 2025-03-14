d9482eb4dc45a1df9426925d9237eab2b6baff07
Mal <anityakumala@gmail.com>
2025-03-14 18:01:16 +0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,18 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <main>
+                <h1>Selamat Datang di CMS Pendidikan</h1>
+                {/* Konten lainnya */}
+            </main>
+            <Footer />
+        </div>
+    );
+};
+
+export default App;
