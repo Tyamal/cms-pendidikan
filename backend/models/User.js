0c4489ae682a04d36f4f8ab92290ba392e99684b
Mal <anityakumala@gmail.com>
2025-03-14 18:05:34 +0700

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,3 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true },
     password: { type: String, required: true },
+    role: { type: String, enum: ['admin', 'guru', 'siswa'], required: true },
+});
+
+module.exports = mongoose.model('User ', userSchema);
