f4c7953d84d9f58e1ae50c4de2d344b8335c5b86
Mal <anityakumala@gmail.com>
2025-03-14 18:04:29 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,3 +4,20 @@ const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+
+// Koneksi ke MongoDB
+mongoose.connect('mongodb://localhost:27017/cms_pendidikan', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+// Rute
+app.use('/api/users', userRoutes);
+
+// Jalankan server
+app.listen(PORT, () => {
+    console.log(`Server berjalan di http://localhost:${PORT}`);
+});
